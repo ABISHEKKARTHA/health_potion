74a7033d101af931030ebe0ecf1934f44ad7f09d
ABISHEKKARTHA <67997045+ABISHEKKARTHA@users.noreply.github.com>
2024-05-15 10:16:01 +0530

    Update health.py
    new while loop

--- health.py
+++ health.py
@@ -2,13 +2,18 @@ import random
 
 health = 50
 
-user_input = int(input("Choose Difficulty: "))
+while True:
+    try:
+        user_input = int(input("Choose Difficulty (1-3): "))
 
-if user_input > 3:
-    raise ValueError("Input must not be greater than 3")
-elif user_input < 0:
-    raise ValueError("Input must not be less than 0")
+        if user_input > 3:
+            raise ValueError("Input must not be greater than 3")
 
+        elif user_input < 1:
+            raise ValueError("Input must not be less than 0")
+        break
+    except ValueError as e:
+        print(e)    
 #Highest difficulty is 3
 #difficulty = 1
     
